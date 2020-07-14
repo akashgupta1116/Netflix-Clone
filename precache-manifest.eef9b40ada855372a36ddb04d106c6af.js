3f397b739e254a6a0adf405b9d770b49c5702cba
akashgupta1116 <yoakash6@gmail.com>
2020-07-14 14:25:24 +0530

    Updates

---
+++ precache-manifest.eef9b40ada855372a36ddb04d106c6af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af7783f714963ceabaa4768542ae28a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cb2312499c23b1ea827",
+    "url": "/static/css/main.99e3aba5.chunk.css"
+  },
+  {
+    "revision": "9096b483d14e194eeb76",
+    "url": "/static/js/2.b613620d.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.b613620d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb2312499c23b1ea827",
+    "url": "/static/js/main.5f90e7ef.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
